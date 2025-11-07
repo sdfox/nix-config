1c411478b448762390687c2eb01093b22792d1d5
Zi Ran <sdfox05@gmail.com>
2025-11-07 18:19:12 +0800

    add riscv cross compile tools
    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -23,6 +23,18 @@
           # Optionally, use home-manager.extraSpecialArgs to pass
           # arguments to home.nix
         }
+        # RISC-V cross compile support
+        ({ config, pkgs, lib, ... }: {
+          environment.systemPackages = with pkgs.pkgsCross.riscv64.buildPackages; [
+            gcc
+            gdb
+            binutils
+          ] ++ [ pkgs.qemu ];
+
+          environment.variables = {
+            CROSS_COMPILE = "riscv64-unknown-linux-gnu-";
+          };
+        })
       ];
     };
   };
