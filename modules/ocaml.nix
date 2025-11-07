7666f456d44e10f3c60e839ef5bef3a00b523ac9
Zi Ran <sdfox05@gmail.com>
2025-11-07 16:15:59 +0800

    modified:   modules/ocaml.nix

--- modules/ocaml.nix
+++ modules/ocaml.nix
@@ -1,7 +1,18 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  ocamlPkgs = pkgs.ocaml-ng.ocamlPackages_5_3;
+in
 {
   environment.systemPackages = with pkgs; [
     opam
     ocaml
-  ];
+  ] ++ (with ocamlPkgs; [
+    ocaml-lsp
+    ocamlformat
+    odoc
+    utop
+  ]);
+
+  environment.variables = {
+  };
 }
